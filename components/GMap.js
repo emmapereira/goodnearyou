732fb2d4708cb4f155cfe76e8934d851bccad777
emma <emmapereira23@gmail.com>
2021-10-14 12:03:03 +0200

    map with markers!

--- components/GMap.js
+++ components/GMap.js
@@ -1,27 +1,72 @@
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api"
+import stores from '../data/stores.json'
+import React, {useState} from "react"
 
-function Map() {
-    return ( 
-        <GoogleMap 
-            defaultZoom={8} 
-            defaultCenter = {{lat: -34.397, lng: 150.644}}
-        />
-     );
+
+const libraries = ["places"]
+
+const mapContainerStyle = {
+    width:"70vw", 
+    height: "70vh"
 }
 
-const wrappedMap = withScriptjs(withGoogleMap(Map));
+const center = {
+    lat: 57.6995819, 
+    lng: 11.9144126
+}
 
-const GMap = () => {
-    return ( 
-        <div style={{width:"100vw", height: "100vh"}} >
-        <wrappedMap 
-        googleMapURL={'https://maps.googleapis.com/maps/api/js?key=AIzaSyCi0xsW1SJeoMI75dm4BRz01ab-ryQR0mg&callback=initMap'}
-        loadingElement={<div style={{height: "100%"}} /> }
-        containerElement={<div style={{height: "100%"}} /> }
-        mapElement={<div style={{height: "100%"}} /> }
-        />
+/*const options = {
+    styles:
+    disableDefaultUI: true,
+    zoomControl: true,
+}*/
+
+const GMap = () => { 
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: "AIzaSyCi0xsW1SJeoMI75dm4BRz01ab-ryQR0mg",            
+        libraries
+    })
+
+    const [selected, setSelected] = React.useState(null)
+    const mapRef = React.useRef();
+    const onMapLoad = React.useCallback((map) => {
+        mapRef.current = map;
+    }, []);
+
+    if (loadError) return "Error loading maps"
+    if (!isLoaded) return "Loading Maps"
+
+    return (
+        <div>
+            <GoogleMap 
+                mapContainerStyle={mapContainerStyle} 
+                zoom={12} 
+                center = {center}
+                //options = {options}
+                onLoad={onMapLoad}
+            >
+             
+            {stores.map((store) => ( 
+                <Marker 
+                    key = {store.id}
+                    position = {{
+                        lat: store.latitude,
+                        lng: store.longitude
+                    }}
+                    onClick = {() => {
+                        setSelected(store)
+                    }}
+                />
+            ))}
+
+            {selected ? (<InfoWindow position={{ lat: selected.lat, lng: selected.lng}}> 
+                <div>
+                    <h2>hey there</h2>
+                </div>
+            </InfoWindow>) : null}
+            </GoogleMap>
         </div>
-     );
+    );
 }
  
 export default GMap;
--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-
-import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps"
-
-function Map() {
-    return ( 
-        <GoogleMap 
-            defaultZoom={10} 
-            defaultCenter = {{ lat: 45.4211, lng: -75.6903 }}
-        />
-     );
-}
-
-const MapWrapped = withScriptjs(withGoogleMap(Map));
+import GMap from '../components/GMap'
 
 export default function Home() {
   return (
@@ -31,14 +18,7 @@ export default function Home() {
           <a className={styles.btn}>See the map with the stores</a>
         </Link>
       </div>
-      <div style={{width:"70vw", height: "70vh"}} >
-          <MapWrapped 
-          googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCi0xsW1SJeoMI75dm4BRz01ab-ryQR0mg'}
-          loadingElement={<div style={{height: '100%'}} /> }
-          containerElement={<div style={{height: '100%'}} /> }
-          mapElement={<div style={{height: '100%'}} /> }
-          />
-        </div>
+      <GMap/>
     </>
   )
 }
