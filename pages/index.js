9fba51b5aea970af2f9c5f7b67f3a0ca61cc6ccf
emma <emmapereira23@gmail.com>
2021-10-19 15:51:31 +0200

    instagram feed is done (footer still wrong)

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import GMap from '../components/GMap'
+import InstagramFeed from '../components/InstagramFeed'
 
 export default function Home() {
   return (
@@ -14,11 +15,13 @@ export default function Home() {
       <div>
         <h1 className={styles.title}>Homepage</h1>
         <p className={styles.text}>This is the homepage of our project welcome to Good Near You</p>
-        <Link href="/map">
-          <a className={styles.btn}>See the map with the stores</a>
-        </Link>
       </div>
       <GMap/>
+      <InstagramFeed/>
     </>
   )
 }
+
+/*<Link href="/map">
+    <a className={styles.btn}>See the map with the stores</a>
+</Link>*/
