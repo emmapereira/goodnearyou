ec95bf676a9ad5a332085dac4bb3e6b85f4284c0
emma <emmapereira23@gmail.com>
2021-10-20 11:13:39 +0200

    recommended stores

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import GMap from '../components/GMap'
 import InstagramFeed from '../components/InstagramFeed'
+import Recommended from '../components/Recommended'
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
         <p className={styles.text}>This is the homepage of our project welcome to Good Near You</p>
       </div>
       <GMap/>
+      <Recommended/>
       <InstagramFeed/>
     </>
   )
